defd446667a849a4e3afd95c3dacd01750a158e5
Balla Pranava Chaitanya <pranavachaitanyaballa@gmail.com>
2025-03-22 10:13:36 +0530

    upto memory

---
+++ execute.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include "memory.h"
+using namespace std;
+int main()
+{
+    Memory current;
+    cout << "default values of memory locations is :00 H" << endl;
+
+    cout << "Enter Memory Location , value to be updated as a pair and press 2 to stop\n";
+
+    while (1)
+    {
+        string A, B;
+        cin >> A;
+        if (A == "2")
+            break;
+        cin >> B;
+        current.Update(A, B);
+    }
+
+    /*
+
+     here will the execution of the code
+
+     will do it later
+    */
+
+    cout << "\nExecution Successful!!\nEnter Memory Location To Get the Updated Values Or press 2 to STOP\n";
+    while (1)
+    {
+        string str;
+        cin >> str;
+
+        if (str == "2")
+            break;
+
+        cout << "value at " << str << " is " << current.get(str) << endl;
+    }
+
+    cout << "Press 3 to View the Entire Non-Zero Memory Locations Or press any other key to stop the program:";
+    string temp;
+    cin >> temp;
+    if (temp == "3")
+    {
+        current.PrintEntireMemory();
+    }
+    cout << "\n program terminated !!!\n";
+}
---
+++ memory.h
@@ -0,0 +1,72 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Memory
+{
+private:
+    map<string, int> data;
+    int A, B, C, D, E, F;
+
+public:
+    Memory()
+    {
+        A = B = C = D = E = F = 0;
+    }
+
+    int HexDigitToDecimal(char temp)
+    {
+        if (isdigit(temp))
+        {
+            return int(temp - '0');
+        }
+        return (10 + (temp - 'A'));
+    }
+
+    void Update(string str, string value)
+    {
+        int val = HexDigitToDecimal(value[0]) * 16 + HexDigitToDecimal(value[1]);
+        data[str] = val;
+    }
+
+    string get(string str)
+    {
+        int value = data[str];
+        string just;
+        int va = value / 16;
+        if (va < 10)
+        {
+            just += ('0' + va);
+        }
+        else
+        {
+            va -= 10;
+            just += ('A' + va);
+        }
+        va = value % 16;
+
+        if (va < 10)
+        {
+            just += ('0' + va);
+        }
+        else
+        {
+            va -= 10;
+            just += ('A' + va);
+        }
+
+        return just;
+    }
+
+    void PrintEntireMemory()
+    {
+        for (auto pos : data)
+        {
+            cout << "value at memory location " << pos.first<< " is as : " << get(pos.first) << endl;
+        }
+    }
+};
+
+#endif
